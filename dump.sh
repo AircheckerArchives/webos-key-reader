1e9383b314b0d99d5ccf15831b7754d324c52890
AircheckerArchives <146817725+AircheckerArchives@users.noreply.github.com>
2026-06-16 21:05:56 +0800

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -8,7 +8,7 @@ cd "${WORKDIR}"
 
 if [ ! -f "gdb" ]; then
 >&2 echo "[*] Obtaining GDB..."
-curl -sS -L -o gdb "https://github.com/lslx/arm-gdb-static/raw/master/gdb76"
+curl -sS -L -o gdb "https://github.com/therealsaumil/static-arm-bins/raw/refs/heads/master/gdb-arm-static-7.11"
 chmod +x gdb
 fi
 
